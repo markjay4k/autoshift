b487940dbef28199449e6ec27531bb997eb31ec8
mark <markjmitry@gmail.com>
2024-01-01 08:18:21 +0000

    update cron shabanger

--- cron_schedule.sh
+++ cron_schedule.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-# script to start crontab command
+#!/bin/sh
 
 COMMAND="/usr/local/bin/python /app/msync.py --loglevel $LOG_LEVEL --verbose >> /var/log/cron.log 2>&1"
 (crontab -l 2>/dev/null; echo "$CRON_SCHEDULE $COMMAND") | crontab -
